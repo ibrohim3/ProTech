daff8f37096c70d90a0beee29c49f6b328e0ced7
ibrohim3 <ibrohim.2128382@gmail.com>
2025-07-19 11:51:29 +0500

    new change

--- main.js
+++ main.js
@@ -2,47 +2,89 @@ const sidebar = [
   {
     id: 1,
     title: 'Home',
-    icon: 'home',
+    icon: './images/icon1.png',
     link: '/',
   },
   {
     id: 2,
-    title: 'Products',
-    icon: 'storefront',
-    link: './style.css',
+    title: 'Discover',
+    icon: './images/icon1.png',
+    link: '#',
   },
   {
     id: 3,
-    title: 'About Us',
-    icon: 'info',
-    link: '/about',
+    title: 'Popular Products',
+    icon: './images/icon2.png',
+    link: '#',
   },
   {
     id: 4,
-    title: 'Contact',
-    icon: 'contact_mail',
-    link: '/contact',
+    title: 'Top Authors',
+    icon: './images/icon3.png',
+    link: '/about',
   },
   {
     id: 5,
-    title: 'Blog',
-    icon: 'article',
+    title: 'Feed',
+    icon: './images/icon4.png',
+    link: '/contact',
+  },
+  {
+    id: 6,
+    title: 'Contact',
+    icon: './images/icon5.png',
     link: '/blog',
   },
 ];
-
 function generateSidebar() {
   const sidebarContainer = document.getElementById('sidebar-menu');
 
   if (!sidebarContainer) return;
 
-  sidebar.forEach((item) => {
+  sidebar.forEach((item, index) => {
     const li = document.createElement('li');
+
     li.innerHTML = `
-       <i class="material-icons">${item.icon}</i>
-       <a href="${item.link}" style="color: white; ">${item.title}</a>
-     `;
+    <div class="div">
+    <img src="${item.icon}" alt="">
+    <a href="${item.link}">${item.title}</a>
+    </div>
+    `;
+
+    if (index === 1) {
+      li.addEventListener('click', function (e) {
+        e.preventDefault();
+        const existingExtra = li.querySelector('.extra-info');
+        if (existingExtra) {
+          existingExtra.remove();
+          return;
+        }
+
+        const extra = document.createElement('div');
+        extra.className = 'extra-info';
+        extra.innerHTML = `
+        <div class="div">
+        Electronics
+        </div>
+          <div class="div">
+        Clothes
+        </div>
+          <div class="div">
+        Bags
+        </div>
+          <div class="div">
+        Food
+        </div>
+        `;
+        li.appendChild(extra);
+      });
+    }
+
     sidebarContainer.appendChild(li);
+    // const div = document.querySelector('div')
+    // div.addEventListener('click', function() {
+    //   div.style = 'border-left: 5px solid #ffc831'
+    // })
   });
 }
 
@@ -102,98 +144,99 @@ let category = [
 ];
 
 function getcategory() {
-  const categoryContainer = document.getElementById('category-menu')
+  const categoryContainer = document.getElementById('category-menu');
 
   if (!categoryContainer) return;
   category.forEach((item) => {
-    const li = document.createElement('li')
+    const li = document.createElement('li');
     li.innerHTML = `
-     <a href="${item.link}"  ">${item.title}</a>
-    `
-    categoryContainer.appendChild(li)
-  })
+     <a href="#"  ">${item.title}</a>
+    `;
+    categoryContainer.appendChild(li);
+  });
 }
-getcategory()
+getcategory();
 
-
-// Get Cards 
+// Get Cards
 
 const products = [
   {
-    productImg: "./images/img1.png",
-    avatar: "./images/avatar.png",
-    title: "MacBook Pro 13” Big Discount",
-    shopName: "Eshop Spot",
+    productImg: './images/img1.png',
+    avatar: './images/avatar.png',
+    title: 'MacBook Pro 13” Big Discount',
+    shopName: 'Eshop Spot',
     price: 1.999552,
     oldPrice: 5415,
     sold: 5,
-    rating: 4.8
+    rating: 4.8,
   },
   {
-    productImg: "./images/img2.png",
-    avatar: "./images/avatar.png",
-    title: "Joystick Game Controller",
-    shopName: "AudioMax",
+    productImg: './images/img2.png',
+    avatar: './images/avatar.png',
+    title: 'Joystick Game Controller',
+    shopName: 'AudioMax',
     price: 29.99,
     oldPrice: 59.99,
     sold: 250,
-    rating: 4.6
+    rating: 4.6,
   },
   {
-    productImg: "./images/img3.png",
-    avatar: "./images/avatar.png",
-    title: "Gaming Headset",
-    shopName: "GamerPro",
+    productImg: './images/img3.png',
+    avatar: './images/avatar.png',
+    title: 'Gaming Headset',
+    shopName: 'GamerPro',
     price: 69.99,
     oldPrice: 89.99,
     sold: 90,
-    rating: 4.9
+    rating: 4.9,
   },
   {
-    productImg: "./images/img4.png",
-    avatar: "./images/avatar.png",
-    title: "Gaming Headset",
-    shopName: "GamerPro",
+    productImg: './images/img4.png',
+    avatar: './images/avatar.png',
+    title: 'Gaming Headset',
+    shopName: 'GamerPro',
     price: 69.99,
     oldPrice: 89.99,
     sold: 90,
-    rating: 4.9
+    rating: 4.9,
   },
   {
-    productImg: "./images/img5.png",
-    avatar: "./images/avatar.png",
-    title: "Gaming Headset",
-    shopName: "GamerPro",
+    productImg: './images/img5.png',
+    avatar: './images/avatar.png',
+    title: 'Gaming Headset',
+    shopName: 'GamerPro',
     price: 69.99,
     oldPrice: 89.99,
     sold: 90,
-    rating: 4.9
+    rating: 4.9,
   },
   {
-    productImg: "./images/img6.png",
-    avatar: "./images/avatar.png",
-    title: "Gaming Headset",
-    shopName: "GamerPro",
+    productImg: './images/img6.png',
+    avatar: './images/avatar.png',
+    title: 'Gaming Headset',
+    shopName: 'GamerPro',
     price: 69.99,
     oldPrice: 89.99,
     sold: 90,
-    rating: 4.9
-  }
+    rating: 4.9,
+  },
 ];
 
 function generateProducts() {
-  const container = document.getElementById("productsContainer");
-  container.innerHTML = "";
+  const container = document.getElementById('productsContainer');
+  container.innerHTML = '';
   products.forEach((item, index) => {
-    const productDiv = document.createElement("div");
-    productDiv.className = "product";
+    const productDiv = document.createElement('div');
+    productDiv.className = 'product';
     productDiv.innerHTML = `
       <div class="product-img">
          <img src="${item.productImg}" alt="${item.title}">
       </div>
     <div class="description">
         <div class="avatar">
-          <img src="${item.avatar}" alt="${item.shopName}" style="width: 40px; height: 40px; border-radius: 50%;">
+          <img src="${item.avatar}" alt="${
+      item.shopName
+    }" style="width: 40px; height: 40px; border-radius: 50%;">
         </div>
         <div class="info">
         <p class="title">${item.title}</p>
@@ -203,7 +246,9 @@ function generateProducts() {
           <div class="prices">
           <div>
           <strong class="price">$${item.price.toFixed(2)}</strong>
-          <s class="old-price" style="text-decoration: line-through;">$${item.oldPrice.toFixed(2)}</s>
+          <s class="old-price" style="text-decoration: line-through;">$${item.oldPrice.toFixed(
+            2
+          )}</s>
           </div>
           <div>
           <s class="sale-number">${item.sold} sold</s>
@@ -219,4 +264,4 @@ function generateProducts() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", generateProducts);
+document.addEventListener('DOMContentLoaded', generateProducts);
